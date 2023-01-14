cd0660c3f016b061d20e41fe24d5844da518c165
7007singh <shw12021982@gmail.com>
2023-01-14 10:53:03 +0530

    [Shweta] Added: SwitchCase funcanility

--- CsharpBasicPrograms/Program.cs
+++ CsharpBasicPrograms/Program.cs
@@ -12,22 +12,39 @@ namespace CsharpBasicPrograms
         {
             Console.WriteLine("Welcome to Csharp basic programs");
 
-            Console.WriteLine("Please Enter first number");
-            int first = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please Enter second number");
-            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please choose any one program from below option");
+            Console.WriteLine(  "1:Calculator\n2:SwitchCase\n10:Exit");
+            int option = Convert.ToInt32(Console.ReadLine());
 
-            Calculator calculator1 = new Calculator(first, second);
-            calculator1.Addition();
-            calculator1.Substraction();
+            switch(option)
+            {
+                case 1:
+                        Console.WriteLine("Please Enter first number");
+                    int first = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please Enter second number");
+                    int second = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please Enter first number");
-            int third = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please Enter second number");
-            int fourth = Convert.ToInt32(Console.ReadLine());
-            Calculator calculator2= new Calculator(third, fourth);
-            calculator2.Addition();
-            Console.ReadLine();
+                    Calculator calculator1 = new Calculator(first, second);
+                    calculator1.Addition();
+                    calculator1.Substraction();
+
+                    Console.WriteLine("Please Enter first number");
+                    int third = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please Enter second number");
+                    int fourth = Convert.ToInt32(Console.ReadLine());
+
+                    Calculator calculator2 = new Calculator(third, fourth);
+                    calculator2.Addition();
+                    break;
+                 case 2:
+                    Console.WriteLine("Please choose a number 'from 1 to 7'");
+                    int day = Convert.ToInt32(Console.ReadLine());
+                    WeekDays.FindADayWhichFallOnWeek(day);
+                    break;
+                 case 10:
+                    Console.WriteLine("Exit");
+                    break;
+            }
         }
     }
 }
---
+++ CsharpBasicPrograms/WeekDays.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBasicPrograms
+{
+    internal class WeekDays
+    {
+        public static void FindADayWhichFallOnWeek(int dayNumber)
+        {
+            switch(dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("Its a Monday");
+                    break;
+                case 2: 
+                        Console.WriteLine("Its a Tuesday");
+                    break;
+                case 3: 
+                        Console.WriteLine("Its a Wednesday");
+                    break;
+                case 4: 
+                        Console.WriteLine("Its a Thrusday");
+                    break;
+                case 5: 
+                        Console.WriteLine("Its a Friday");
+                    break;
+                case 6:
+                        Console.WriteLine("Its a Saturday");
+                    break;
+                case 7: 
+                        Console.WriteLine("Its a Sunday");
+                    break;
+                default: 
+                        Console.WriteLine("Please enter a number between 1 to 7");
+                    break;
+            }
+        }
+    }
+}
